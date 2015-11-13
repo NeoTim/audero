71ae59c03731a1807993ebdb8c03ca838cdc74ce
Aurelio De Rosa <aurelioderosa@gmail.com>
2015-11-13 03:36:48 +0000

    Removed fastclick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,6 @@
 var isModernBrowser = 'querySelector' in document && 'localStorage' in window && 'addEventListener' in window;
 
 if (isModernBrowser) {
-   require('picturefill');
    require('classlist-polyfill');
 
    var Audero = require('./audero');
